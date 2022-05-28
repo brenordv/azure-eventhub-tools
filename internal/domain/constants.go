a66eaaf5d8f8d06d0ec483c6187ac3ef8692b98c
Breno RdV <noreply@raccoon.ninja>
2022-05-28 03:12:00 -0300

    bumping version number

--- internal/domain/constants.go
+++ internal/domain/constants.go
@@ -1,7 +1,7 @@
 package domain
 
 const (
-	Version                       = "2.0.3-RC"
+	Version                       = "2.0.3"
 	AppDataFolder                 = ".app-data"
 	ConfigsFolder                 = ".configs"
 	OutboundFolder                = ".outbound"
