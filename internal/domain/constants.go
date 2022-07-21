15a71ff3430deccd6dea71468cbf59df2e017bcb
Breno RdV <noreply@raccoon.ninja>
2022-07-21 14:56:23 -0300

    updating version number

--- internal/domain/constants.go
+++ internal/domain/constants.go
@@ -1,7 +1,7 @@
 package domain
 
 const (
-	Version                       = "2.0.4"
+	Version                       = "2.0.4.RC"
 	AppDataFolder                 = ".app-data"
 	ConfigsFolder                 = ".configs"
 	OutboundFolder                = ".outbound"
